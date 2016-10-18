cb08abde678b1f1cd25b9f870c43290e63656861
Paul Demeulenaere <pauld@unity3d.com>
2016-10-18 16:13:56 +0200

    Add SymLink helper and ignore duplicated files from this repository (maybe is there a better way ?)

---
+++ _ExternalSymLink.sh
@@ -0,0 +1,4 @@
+cd Assets
+ln -s ../External/MaterialGraphProject/Assets/GraphFramework GraphFramework
+ln -s ../External/MaterialGraphProject/Assets/NewUI NewUI
+ln -s ../External/MaterialGraphProject/Assets/UnityShaderEditor UnityShaderEditor
