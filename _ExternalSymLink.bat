72196d7e3869c18dde28c6b2a5d77dc103afedd4
Paul Demeulenaere <pauld@unity3d.com>
2016-10-19 15:57:40 +0200

    Update MaterialGraph submodule and fix symlink

---
+++ _ExternalSymLink.bat
@@ -0,0 +1,4 @@
+mklink /J "Assets\UnityShaderEditor" "%cd%\External\MaterialGraphProject\Assets\UnityShaderEditor"
+mklink /J "Assets\NewUI" "%cd%\External\MaterialGraphProject\Assets\NewUI"
+mklink /J "Assets\GraphFramework" "%cd%\External\MaterialGraphProject\Assets\GraphFramework"
+pause
--- _ExternalSymLink.sh
+++
@@ -1,4 +0,0 @@
-cd Assets
-ln -s ../External/MaterialGraphProject/Assets/GraphFramework GraphFramework
-ln -s ../External/MaterialGraphProject/Assets/NewUI NewUI
-ln -s ../External/MaterialGraphProject/Assets/UnityShaderEditor UnityShaderEditor
