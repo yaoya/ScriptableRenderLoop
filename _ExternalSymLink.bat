eff3c7107e4308bc9b5655a896ad8ac0007e20a8
Paul Demeulenaere <pauld@unity3d.com>
2016-10-19 16:05:42 +0200

    Fix windows symlink batch (temporary tool)

--- _ExternalSymLink.bat
+++ _ExternalSymLink.bat
@@ -1,4 +1,4 @@
-mklink /J "Assets\UnityShaderEditor" "%cd%\External\MaterialGraphProject\Assets\UnityShaderEditor"
-mklink /J "Assets\NewUI" "%cd%\External\MaterialGraphProject\Assets\NewUI"
-mklink /J "Assets\GraphFramework" "%cd%\External\MaterialGraphProject\Assets\GraphFramework"
+mklink /J "%cd%\Assets\UnityShaderEditor" "%cd%\External\MaterialGraphProject\Assets\UnityShaderEditor"
+mklink /J "%cd%\Assets\NewUI" "%cd%\External\MaterialGraphProject\Assets\NewUI"
+mklink /J "%cd%\Assets\GraphFramework" "%cd%\External\MaterialGraphProject\Assets\GraphFramework"
 pause
